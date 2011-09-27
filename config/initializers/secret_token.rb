4f0ba0e8aa3d67d3510ad0f5df0ce4af042346b3
Alexander <iosdemo@reStore-DemoCPU-7.local>
2011-09-27 14:54:25 +0400

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4e01f3051d6e468f7e76195066754c6971a70a6f7b87890bf083a4a397de10d30b4e4934ad36cbd4089f9ecda5dbc5ba8e1907bf6d2845962f41da064ceb5c39'
